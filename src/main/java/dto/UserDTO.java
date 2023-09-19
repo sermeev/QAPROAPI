d8787aa50bae71186e8b6cd472486bc045d2dcb7
Mikhail Sermeev <m.sermeev@ roseltorg.ru>
2023-09-19 23:06:27 +0300

    Добавление тестов

---
+++ src/main/java/dto/UserDTO.java
@@ -0,0 +1,21 @@
+
+package dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+public class UserDTO {
+    private String email;
+    private String firstName;
+    private Long id;
+    private String lastName;
+    private String password;
+    private String phone;
+    private Long userStatus;
+    private String username;
+}
